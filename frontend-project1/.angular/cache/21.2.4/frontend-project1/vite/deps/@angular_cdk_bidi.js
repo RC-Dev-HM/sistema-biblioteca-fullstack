dd735b7c65942d08d9721ffdaa975753705435ef
RC-Dev-HM <robe10hema@gmail.com>
2026-03-26 17:51:46 -0600

    Add selected projects

---
+++ frontend-project1/.angular/cache/21.2.4/frontend-project1/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-W7GNL624.js";
+import "./chunk-J2K5FKRI.js";
+import "./chunk-H2SRQSE4.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ frontend-project1/.angular/cache/21.2.4/frontend-project1/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,18 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-7SHMM5W5.js";
+import "./chunk-M4QRPOQQ.js";
+import "./chunk-HCYSEQZL.js";
+import "./chunk-C2LTKCZO.js";
+import "./chunk-PHBZXMQI.js";
+import "./chunk-J2K5FKRI.js";
+import "./chunk-H2SRQSE4.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
